a215352d28fc85609515e698c945685a13fdcf83
Vasyl20 <57089606+Vasyl20@users.noreply.github.com>
2021-06-03 10:39:38 +0300

    Карти

---
+++ Лаби/Алгоритми і строктури даних/Алгоритими даних_2/Карти/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Карти
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] cart = new string[4] { "♣", "♠", "♦", "♥" };
+            string[] mast = new string[8] { "6", "7", "8", "9", "J", "Q", "K", "A" };
+            List<string> masttt = new List<string>(8) { "6", "7", "8", "9", "J", "Q", "K", "A" };
+
+            Random random = new Random();
+            
+
+            Console.WriteLine("\n");
+            for (int i = 0; i < 8; i++)
+            {
+                for (int l = 0; l < 4; l++)
+                {
+                    Console.Write("\t" + mast[random.Next(0,8)] + cart[random.Next(0, 4)] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("       ====================================================");
+
+            Console.WriteLine("\n");
+            for (int i = 0; i < 8; i++)
+            {
+                for (int l = 0; l < 4; l++)
+                {
+                    Console.Write("\t"+mast[i]+cart[l]+"\t");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
---
+++ Лаби/Алгоритми і строктури даних/Алгоритими даних_2/Подорож/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Подорож
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            seva1:
+            int a1, a2;
+            Console.WriteLine("Виберiть транспорт:");
+            Console.WriteLine("[1] - Автобус \n" +
+                "[2] - Машиною \n" +
+                "[3] - Велосипедом \n" +
+                "[4] - Пiшки");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            switch (a1)
+            {
+                case 1:
+                    seve2:
+                    Console.Clear();
+                    Console.WriteLine("Якiй ви напрямов виберете ");
+                    Console.WriteLine("[1] - Легкiй \n" +
+                        "[2] - Важкiй");
+                    a2 = Convert.ToInt32(Console.ReadLine());
+                    switch (a2)
+                    {
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        default:
+                            goto seve2;
+                    }
+                    break;
+                    
+                        
+                default:
+                    goto seva1;
+            }
+            
+        }
+    }
+}
